19e6295abe4d8b702b641a7513a83b08b48436fd
ivanrputra <ivanrputra@gmail.com>
2020-07-23 02:51:42 +0700

    remove env gitignore

--- core/admin.py
+++ core/admin.py
@@ -1,3 +1,26 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
+from django.utils.translation import gettext as _
 
-# Register your models here.
+from . import models
+
+class UserAdmin(admin.ModelAdmin):
+	ordering		= ['id']
+	list_display	= ['email','created_at']
+	readonly_fields = ('created_at', )
+
+	fieldsets		= (
+		(None,{'fields':('email','username')}),
+		(_('Personal Info'),{'fields':('firstname','lastname')}),
+		(_('Permissions'),
+			{'fields':('is_active','is_staff','is_superuser')}
+		),
+		(_('Important dates'),{'fields':('last_login',)})
+	)
+
+admin.site.register(models.User,UserAdmin)
+admin.site.register(models.Category)
+admin.site.register(models.Course)
+admin.site.register(models.Session)
+admin.site.register(models.Library)
+
