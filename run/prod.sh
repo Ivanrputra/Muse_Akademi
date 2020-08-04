b0030793cf18792fa708a3a3882cf6e2f81468a8
ivanrputra <ivanrputra@gmail.com>
2020-08-04 11:15:36 +0700

    change heroku

--- run/prod.sh
+++ run/prod.sh
@@ -1,3 +1,3 @@
-python manage.py makemigrations --settings=MuseAcademy.settings_dev
-python manage.py migrate --settings=MuseAcademy.settings_dev
+python manage.py makemigrations --settings=MuseAcademy.settings_prod
+python manage.py migrate --settings=MuseAcademy.settings_prod
 python manage.py runserver --settings=MuseAcademy.settings_prod
