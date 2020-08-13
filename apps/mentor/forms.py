d9c1017558cc2749da2638738814398d02e2650a
fandiarfa26 <arfabuma.987@gmail.com>
2020-08-13 13:19:03 +0700

    fix registration form

--- apps/mentor/forms.py
+++ apps/mentor/forms.py
@@ -1,16 +1,21 @@
 from django import forms
 from django.utils.safestring import mark_safe
 from django.contrib.auth import get_user_model,authenticate
+from django.forms.widgets import ClearableFileInput
 
 from string import Template
 
 from core.models import MentorData,ExamReport
 
+class CustomClearableFileInput(ClearableFileInput):
+	template_name = "ext/input_group_file.html"
+
 # Form for user registration
 class RegisterMentor(forms.ModelForm):
+	ktp		= forms.FileField(widget=CustomClearableFileInput(attrs={}))
 	class Meta():
 		model 	= MentorData
-		fields 	= ('no_ktp','cv','ktp','npwp','certification','portofolio')
+		fields 	= ('no_ktp','ktp','certification','cv','npwp','portofolio')
 
 class ExamReportForm(forms.ModelForm):
 	ide		= forms.IntegerField(min_value=0,max_value=100)
