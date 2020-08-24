71d85e27146419900f5a97272407501b168a3db3
ivanrputra <ivanrputra@gmail.com>
2020-08-25 05:21:26 +0700

    fix serve media erro

--- MuseAcademy/urls.py
+++ MuseAcademy/urls.py
@@ -18,6 +18,7 @@ from django.urls import path,include
 from django.conf.urls.static import static
 from django.conf import settings
 from django.conf.urls.i18n import i18n_patterns 
+from django.views.static import serve
 
 from apps.app.views import change_language
 
