b80e2d7bd238df5bbed50c947ce19464c72035b8
ivanrputra <ivanrputra@gmail.com>
2020-08-25 00:04:02 +0700

    prod setting

--- MuseAcademy/settings_prod.py
+++ MuseAcademy/settings_prod.py
@@ -4,8 +4,8 @@ import dj_database_url
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.mysql', 
-        'NAME': 'museakad_prod',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2', 
+        'NAME': 'museakad_production',
         'USER': 'museakad_prod',
         'PASSWORD': 'digistar_muse23',
         'HOST': 'localhost',   # Or an IP Address that your DB is hosted on
