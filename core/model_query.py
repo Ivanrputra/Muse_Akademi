6ab85ca08f932ae133187096e6ff9f348e742fcb
ivanrputra <ivanrputra@gmail.com>
2020-08-25 11:28:49 +0700

    change active course from now()

--- core/model_query.py
+++ core/model_query.py
@@ -2,7 +2,7 @@ from django.utils import timezone
 from .models import *
 
 def get_active_course():
-    return Course.objects.filter(is_publish=True,start_at__gt=timezone.now().date())
+    return Course.objects.filter(is_publish=True,start_at__gte=timezone.now().date())
 
 def get_all_course():
     return Course.objects.all()
