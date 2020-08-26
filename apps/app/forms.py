40390afb21bb272f270ce45b1096433ba5d79a21
fandiarfa26 <arfabuma.987@gmail.com>
2020-08-26 22:15:30 +0700

    fix form exam answer & project

--- apps/app/forms.py
+++ apps/app/forms.py
@@ -4,11 +4,14 @@ from django import forms
 from core.models import ExamProject,ExamAnswer,Order
 
 class ExamProjectForm(forms.ModelForm):
+	title   	= forms.CharField(widget=forms.TextInput(attrs={'class':'form-control','placeholder':'Judul Tugas'}))
+	url_project = forms.CharField(widget=forms.URLInput(attrs={'class':'form-control','placeholder':'URL File Pengumpulan Tugas'}))
 	class Meta():
 		model 	= ExamProject
 		fields 	= ('title','url_project',)
 
 class ExamAnswerForm(forms.ModelForm):
+	answer		= forms.CharField(widget=forms.Textarea(attrs={'class':'form-control','rows':'5','placeholder':'Deskripsikan tentang tugas yang Anda buat secara singkat...'}))
 	class Meta():
 		model 	= ExamAnswer
 		fields 	= ('answer',)
